ead5866251f67c3074c124f058f13d59fdc3210f
Gino Kanaan <2175202@talnet.nl>
2024-10-18 10:22:35 +0200

    Footer Styled

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -8,3 +8,23 @@
         </div>
     </footer>
   </template>
+
+<style>
+footer {
+    background-color: #ffffff; 
+    border-top: 4px solid #d50000; 
+    padding: 10px 0;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+}
+
+.footer-content {
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 0 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+</style>
